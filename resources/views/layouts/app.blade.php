9b2c3ed4bc4d15e55dba8ffb7aec8404915cba8c
nyCarvajal <nycarvajal@outlook.com>
2026-02-05 16:20:32 -0500

    fix: usar conexión tenant en modelo Appointment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
                 <a href="{{ route('roles.index') }}" class="sidebar__link">Roles</a>
                 <a href="{{ route('permissions.index') }}" class="sidebar__link">Permisos</a>
                 <a href="{{ route('clients.index') }}" class="sidebar__link">Clientes</a>
+                <a href="{{ route('appointments.index') }}" class="sidebar__link">Agenda</a>
             </nav>
         </aside>
         <div class="app-shell__content">
